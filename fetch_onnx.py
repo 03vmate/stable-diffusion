ac7204a1dd846c8fd5c2a5965439347929ea2a5b
Mate V <47510515+03vmate@users.noreply.github.com>
2022-10-02 00:40:45 +0200

    Update fetch_onnx.py

--- fetch_onnx.py
+++ fetch_onnx.py
@@ -3,12 +3,20 @@ AUTH_TOKEN = ""
 import onnx
 import torch
 from diffusers import UNet2DConditionModel
+import sys
+
+width = int(sys.argv[1])
+height = int(sys.argv[2])
+
+assert(width % 8 == 0, "Width should be divisible by 8")
+assert(height % 8 == 0, "Height should be divisible by 8")
+
 
 unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16, revision="fp16", subfolder="unet", use_auth_token=AUTH_TOKEN)
 unet.cuda()
 
 with torch.inference_mode(), torch.autocast("cuda"):
-    inputs = torch.randn(2,4,64,64, dtype=torch.half, device='cuda'), torch.randn(1, dtype=torch.half, device='cuda'), torch.randn(2, 77, 768, dtype=torch.half, device='cuda')
+    inputs = torch.randn(2, 4, height // 8, width // 8, dtype=torch.half, device='cuda'), torch.randn(1, dtype=torch.half, device='cuda'), torch.randn(2, 77, 768, dtype=torch.half, device='cuda')
 
     # Export the model
     torch.onnx.export(unet,               # model being run
